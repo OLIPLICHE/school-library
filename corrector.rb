5c377ee33cfced5334dce6802bb30e6f35693393
olipliche <olipliche@gmail.com>
2021-11-17 17:32:19 +0100

    Update corrector class

--- corrector.rb
+++ corrector.rb
@@ -1,2 +1,11 @@
 class Corrector
+  def correct_name(name)
+    get_name = name.strip.capitalize
+
+    if get_name.length > 10
+        get_name[0, 10]
+    else
+        get_name
+    end
+  end
 end
