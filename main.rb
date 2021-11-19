ab7a6207f49e07261298b7277246459763ad61bd
olipliche <olipliche@gmail.com>
2021-11-19 04:32:43 +0100

    update main

--- main.rb
+++ main.rb
@@ -7,6 +7,20 @@ require './student'
 require './teacher'
 
 class App
+  attr_accessor :books, :persons, :rentals
+
+  def initialize
+    @books = []
+    @people = []
+    @rentals = []
+    @class = Classroom.new('Grade 7')
+  end
+
+  def run
+    print 'Welcome to School Library App!'
+    sleep 0.75
+    menu
+  end
 end
 
 def main
