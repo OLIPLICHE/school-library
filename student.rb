82e263bf2c05f6775cddf7ef8af7493a30c38529
olipliche <olipliche@gmail.com>
2021-11-19 11:45:05 +0100

    change parent_permission sintax inside student file

--- student.rb
+++ student.rb
@@ -1,8 +1,9 @@
+# rubocop: disable Style/OptionalBooleanParameter
 require './person'
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission = true)
     super(name, age, parent_permission)
     @classroom = classroom
   end
@@ -16,3 +17,4 @@ class Student < Person
     classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
+# rubocop: enable Style/OptionalBooleanParameter
