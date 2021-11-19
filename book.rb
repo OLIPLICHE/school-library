071224de3c8f088db17071bd83a5f7411b5a64ba
olipliche <olipliche@gmail.com>
2021-11-19 02:01:42 +0100

    create book.rb

---
+++ book.rb
@@ -0,0 +1,15 @@
+class Book
+  attr_accessor :title, :author
+  attr_reader :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals << rental
+    rental.book = self
+  end
+end
